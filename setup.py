c9b48ca62c9b4a03cb2ea910c3e148bcaa23c1b6
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2021-10-01 17:01:39 -0300

    Modify setup

--- setup.py
+++ setup.py
@@ -1,27 +1,8 @@
-import setuptools
-
-with open('README.md', 'r', encoding='utf-8') as fh:
-    long_description = fh.read()
+from setuptools import setup
 
 
-setuptools.setup(
-    name="btseg",
-    version="0.0.1",
-    author="Gilberto Medeiros",
-    author_email="medeiros.gilberto.br@gmail.com",
-    description="",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jose-gilberto/btseg",
-    project_urls={
-        "Bug Tracker": "https://github.com/jose-gilberto/btseg/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "btseg"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+setup(
+    name='btseg',
+    version='0.0.1',
+    packages=['btseg']
 )
