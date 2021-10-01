e11e9955993216b47583413c5b71c906697970bd
Gilberto Barbosa <medeiros.gilberto.br@gmail.com>
2021-10-01 17:01:49 -0300

    Update transformers init module

--- btseg/transformers/__init__.py
+++ btseg/transformers/__init__.py
@@ -1,3 +1,13 @@
 from .compose import Compose
-from .image_transformers import MinimumBoundingBox, Resize, SegmentExtraction
+from .image_transformers import (
+    MinimumBoundingBox,
+    Resize,
+    SegmentExtraction,
+    CannyEdge,
+    ConvertColor,
+    Dilate,
+    Erode,
+    GaussianBlur,
+    RegionSelection
+)
 from .nii_transformers import NiiToImage
